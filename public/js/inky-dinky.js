dd6262194a869b55727a80ed7d1e5e0ead984d91
Sara Tan <saratan@mac.com>
2018-10-21 12:04:36 +0100

    Setup PWA

--- public/js/inky-dinky.js
+++ public/js/inky-dinky.js
@@ -126,3 +126,9 @@ function showOrHideCredits() {
     element.innerHTML = credits;
   }
 }
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('./service-worker.js')
+           .then(function() { console.log('Service Worker Registered'); });
+}
